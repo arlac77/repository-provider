f15a587eb452ba534b9a9270b11f117950d6b0b1
Markus Felten <Markus.Felten@gmx.de>
2017-11-14 22:51:05 +0100

    fix: export github,bitbucket and local provider too

---
+++ src/module.js
@@ -0,0 +1,38 @@
+import {
+  Provider,
+  Repository,
+  Branch,
+  PullRequest
+} from './repository-provider';
+
+import {
+  LocalProvider,
+  LocalRepository,
+  LocalBranch
+} from './local-repository-provider';
+import {
+  GithubProvider,
+  GithubRepository,
+  GithubBranch
+} from './github-repository-provider';
+import {
+  BitbucketProvider,
+  BitbucketRepository,
+  BitbucketBranch
+} from './bitbucket-repository-provider';
+
+export {
+  Provider,
+  Repository,
+  Branch,
+  PullRequest,
+  LocalProvider,
+  LocalRepository,
+  LocalBranch,
+  GithubProvider,
+  GithubRepository,
+  GithubBranch,
+  BitbucketProvider,
+  BitbucketRepository,
+  BitbucketBranch
+};
