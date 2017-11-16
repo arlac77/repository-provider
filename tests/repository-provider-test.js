a4253d8c5b429aa50a79cbbd9951376fb1364de7
Markus Felten <Markus.Felten@gmx.de>
2017-11-16 18:43:47 +0100

    feat: move derived providers into their own module
    BREAKING CHANGE: no longer provider bitbucket, github and local providers
    
    use:
    arlac77/local-repository-provider
    arlac77/bitbucket-repository-provider
    arlac77/github-repository-provider
    modules

---
+++ tests/repository-provider-test.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import { Provider } from '../src/repository-provider';
+
+test('provider config', async t => {
+  const provider = new Provider({ key: 'value' });
+
+  const repository = await provider.config;
+
+  t.deepEqual(provider.config, { key: 'value' });
+});
