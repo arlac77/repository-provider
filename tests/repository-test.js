8f4ed26d65d9d568be138d11c40e33561bf181e5
Markus Felten <Markus.Felten@gmx.de>
2018-08-24 17:30:07 +0200

    test: fullName

--- tests/repository-test.js
+++ tests/repository-test.js
@@ -10,6 +10,7 @@ test("repository create with options", t => {
   });
   t.is(repository.owner, owner);
   t.is(repository.name, "r1");
+  t.is(repository.fullName, "r1");
   t.is(repository.type, "git");
   t.is(repository.description, "a description");
   t.is(repository.id, "4711");
@@ -20,6 +21,7 @@ test("repository create without options", t => {
   const repository = new Repository(owner, "r1");
   t.is(repository.owner, owner);
   t.is(repository.name, "r1");
+  t.is(repository.fullName, "r1");
   t.is(repository.type, "git");
   t.is(repository.description, undefined);
 });
@@ -29,4 +31,5 @@ test("repository normalize name", t => {
   const repository = new Repository(owner, "r1#branch");
   t.is(repository.owner, owner);
   t.is(repository.name, "r1");
+  t.is(repository.fullName, "r1");
 });
