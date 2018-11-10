248c499caa53f5070ca3c22d1b328d0c54e1c195
Markus Felten <Markus.Felten@gmx.de>
2018-11-10 18:00:24 +0100

    fix:get rid of babel

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import json from "rollup-plugin-json";
 import executable from "rollup-plugin-executable";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
 import cleanup from "rollup-plugin-cleanup";
 import pkg from "./package.json";
 
@@ -15,13 +14,6 @@ export default {
   },
   external: ["stream"],
   plugins: [
-    babel({
-      runtimeHelpers: false,
-      externalHelpers: true,
-      babelrc: false,
-      plugins: ["@babel/plugin-proposal-async-generator-functions"],
-      exclude: "node_modules/**"
-    }),
     resolve(),
     commonjs(),
     cleanup()
