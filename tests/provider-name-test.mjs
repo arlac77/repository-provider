03c6ba9b9004e56f803df18e4acb3e91b88922e4
Markus Felten <Markus.Felten@gmx.de>
2020-03-05 17:55:44 +0100

    test: make use of providerParseNameTest

--- tests/provider-name-test.mjs
+++ tests/provider-name-test.mjs
@@ -1,4 +1,6 @@
 import test from "ava";
+import { providerParseNameTest } from "repository-provider-test-support";
+
 import { Provider } from "../src/provider.mjs";
 
 class MyProvider extends Provider {
@@ -19,82 +21,74 @@ test("provider normalize repo name", t => {
   t.is(provider.normalizeRepositoryName("abc/def.git#mybranch"), "abc/def");
 });
 
-test("provider parseName", t => {
-  const provider = new MyProvider();
-  const nameFixtures = {
-    abc: { repository: "abc" },
-    " abc/def": { group: "abc", repository: "def" },
-    "abc/def#mybranch ": {
-      group: "abc",
-      repository: "def",
-      branch: "mybranch"
-    },
-    "abc/def.git": { group: "abc", repository: "def" },
-    "abc/def.git#mybranch": {
-      group: "abc",
-      repository: "def",
-      branch: "mybranch"
-    },
-    "xxx/abc/def.git#mybranch": {
-      group: "abc",
-      repository: "def",
-      branch: "mybranch"
-    },
+test(providerParseNameTest, new MyProvider(), {
+  abc: { repository: "abc" },
+  " abc/def": { group: "abc", repository: "def" },
+  "abc/def#mybranch ": {
+    group: "abc",
+    repository: "def",
+    branch: "mybranch"
+  },
+  "abc/def.git": { group: "abc", repository: "def" },
+  "abc/def.git#mybranch": {
+    group: "abc",
+    repository: "def",
+    branch: "mybranch"
+  },
+  "xxx/abc/def.git#mybranch": {
+    group: "abc",
+    repository: "def",
+    branch: "mybranch"
+  },
 
-    "https://github.com/arlac77/sync-test-repository.git#mybranch": {
-      base: 'https://github.com/',
-      group: "arlac77",
-      repository: "sync-test-repository",
-      branch: "mybranch"
-    },
-    "https://user:password@github.com/arlac77/sync-test-repository.git#mybranch": {
-      base: 'https://github.com/',
-      group: "arlac77",
-      repository: "sync-test-repository",
-      branch: "mybranch"
-    },
-    "https://user@github.com/arlac77/sync-test-repository.git#mybranch": {
-      base: 'https://github.com/',
-      group: "arlac77",
-      repository: "sync-test-repository",
-      branch: "mybranch"
-    },
-    "http://otherdomain.com/arlac77/sync-test-repository.git#mybranch": {
-      base: 'http://otherdomain.com',
-      group: "arlac77",
-      repository: "sync-test-repository",
-      branch: "mybranch"
-    },
-    "git+http://otherdomain.com/arlac77/sync-test-repository.git#mybranch": {
-      base: 'http://otherdomain.com',
-      group: "arlac77",
-      repository: "sync-test-repository",
-      branch: "mybranch"
-    },
-    "git+http://arlac77@otherdomain.com/prefix/arlac77/sync-test-repository.git": {
-      base: 'http://otherdomain.com',
-      group: "arlac77",
-      repository: "sync-test-repository"
-    },
-    "git@bitbucket.org:arlac77/sync-test-repository.git": {
-      base: 'git@bitbucket.org:',
-      group: "arlac77",
-      repository: "sync-test-repository"
-    },
-    "git@bitbucket.org/arlac77/sync-test-repository.git": {
-      base: 'git@bitbucket.org/',
-      group: "arlac77",
-      repository: "sync-test-repository"
-    },
-    "https://arlac77@bitbucket.org/arlac77/sync-test-repository.git" : {
-      base: 'https://bitbucket.org/',
-      group: "arlac77",
-      repository: "sync-test-repository"
-    }
-
-  };
-
-  for (const name of Object.keys(nameFixtures)) {
-    t.deepEqual(provider.parseName(name), nameFixtures[name]);
+  "https://github.com/arlac77/sync-test-repository.git#mybranch": {
+    base: "https://github.com/",
+    group: "arlac77",
+    repository: "sync-test-repository",
+    branch: "mybranch"
+  },
+  "https://user:password@github.com/arlac77/sync-test-repository.git#mybranch": {
+    base: "https://github.com/",
+    group: "arlac77",
+    repository: "sync-test-repository",
+    branch: "mybranch"
+  },
+  "https://user@github.com/arlac77/sync-test-repository.git#mybranch": {
+    base: "https://github.com/",
+    group: "arlac77",
+    repository: "sync-test-repository",
+    branch: "mybranch"
+  },
+  "http://otherdomain.com/arlac77/sync-test-repository.git#mybranch": {
+    base: "http://otherdomain.com",
+    group: "arlac77",
+    repository: "sync-test-repository",
+    branch: "mybranch"
+  },
+  "git+http://otherdomain.com/arlac77/sync-test-repository.git#mybranch": {
+    base: "http://otherdomain.com",
+    group: "arlac77",
+    repository: "sync-test-repository",
+    branch: "mybranch"
+  },
+  "git+http://arlac77@otherdomain.com/prefix/arlac77/sync-test-repository.git": {
+    base: "http://otherdomain.com",
+    group: "arlac77",
+    repository: "sync-test-repository"
+  },
+  "git@bitbucket.org:arlac77/sync-test-repository.git": {
+    base: "git@bitbucket.org:",
+    group: "arlac77",
+    repository: "sync-test-repository"
+  },
+  "git@bitbucket.org/arlac77/sync-test-repository.git": {
+    base: "git@bitbucket.org/",
+    group: "arlac77",
+    repository: "sync-test-repository"
+  },
+  "https://arlac77@bitbucket.org/arlac77/sync-test-repository.git": {
+    base: "https://bitbucket.org/",
+    group: "arlac77",
+    repository: "sync-test-repository"
   }
 });
