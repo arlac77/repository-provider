e36213cec7c95fa82e4159d5478b4bb6f26ac481
Markus Felten <Markus.Felten@gmx.de>
2020-04-07 00:21:16 +0200

    test: some more groupListTest

--- tests/group-list-test.mjs
+++ tests/group-list-test.mjs
@@ -3,9 +3,11 @@ import { groupListTest } from "repository-provider-test-support";
 import { Provider } from "../src/provider.mjs";
 
 const provider = new Provider();
-provider._createRepositoryGroup("g1");
-provider._createRepositoryGroup("g2");
+provider.addRepositoryGroup("g1");
+provider.addRepositoryGroup("g2");
 
 test(groupListTest, provider, undefined, { g1: {}, g2: {}});
 test(groupListTest, provider, "*", { g1: {}, g2: {} });
-test(groupListTest, provider, "g1", { g1: {} });
+test(groupListTest, provider, "g1", 1);
+test(groupListTest, provider, "*2", 1);
+test(groupListTest, provider, "g*", { g1: {}, g2: {} });
