c8a69a066f6b18f2dffbc2562e74b72657104be2
Markus Felten <Markus.Felten@gmx.de>
2020-04-06 04:59:38 +0200

    test: fix import

--- tests/group-list-test.mjs
+++ tests/group-list-test.mjs
@@ -1,11 +1,11 @@
 import test from "ava";
-import { groupListTest } from "repository-provider-test-support/src/group-list-test.mjs";
+import { groupListTest } from "repository-provider-test-support";
 import { Provider } from "../src/provider.mjs";
 
 const provider = new Provider();
 provider._createRepositoryGroup("g1");
 provider._createRepositoryGroup("g2");
 
-test(groupListTest, provider, undefined, { "g1": { name: "g1" } });
-test(groupListTest, provider, "g1", { "g1": { name: "g1" } });
-test(groupListTest, provider, "*", { "g1": { name: "g1" } });
+test(groupListTest, provider, undefined, { g1: {}, g2: {}});
+test(groupListTest, provider, "*", { g1: {}, g2: {} });
+test(groupListTest, provider, "g1", { g1: {} });
