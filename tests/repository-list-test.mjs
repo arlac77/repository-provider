b64db04d3b53cb2179b83150b38be8a69314f2e8
Markus Felten <Markus.Felten@gmx.de>
2020-04-30 21:01:59 +0200

    test: use addRepository for list test

--- tests/repository-list-test.mjs
+++ tests/repository-list-test.mjs
@@ -6,11 +6,10 @@ const provider = new Provider();
 
 test.before(async t => {
   const g1 = await provider.addRepositoryGroup("g1");
-  await g1.createRepository("r1");
-  await g1.createRepository("r2");
+  await g1.addRepository("r1");
+  await g1.addRepository("r2");
   const g2 = await provider.addRepositoryGroup("g2");
-  await g2.createRepository("r1");
-
+  await g2.addRepository("r1");
   const g3 = await provider.addRepositoryGroup("g3");
 });
 
