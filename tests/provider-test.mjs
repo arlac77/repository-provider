7e215ec3662e7a8e3be73fd9974d7cf690d1b6d0
Markus Felten <Markus.Felten@gmx.de>
2020-05-01 02:26:24 +0200

    style: reformat

--- tests/provider-test.mjs
+++ tests/provider-test.mjs
@@ -2,4 +2,4 @@ import test from "ava";
 import { providerTest } from "repository-provider-test-support/src/provider-test.mjs";
 import { Provider } from "../src/provider.mjs";
 
-test(providerTest,new Provider());
+test(providerTest, new Provider());
