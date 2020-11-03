22387dc016a1836813f4a2bfa3e1b0d6f0afa0b5
Markus Felten <Markus.Felten@gmx.de>
2020-11-03 13:35:56 +0100

    test: skeleton Milestone

---
+++ tests/milestone-test.mjs
@@ -0,0 +1,10 @@
+import test from "ava";
+import { Milestone } from "repository-provider";
+
+test("init Milestone", async t => {
+  const owner = { _addMilestone: () => {} };
+  const m = new Milestone(owner, "m1");
+
+  t.is(m.name, "m1");
+  t.is(m.displayName, "m1");
+});
