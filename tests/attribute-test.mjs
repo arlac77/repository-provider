4dee23809c7ca8902aa8cf32e2676f5e94b6550b
Markus Felten <Markus.Felten@gmx.de>
2020-12-08 12:15:14 +0100

    test: some setAttribute edge cases

--- tests/attribute-test.mjs
+++ tests/attribute-test.mjs
@@ -137,11 +137,12 @@ function sat(t, object, key, value, expected) {
   t.deepEqual(object, expected);
 }
 
-sat.title = (providedTitle, object, key, value) =>
+sat.title = (providedTitle, object, key, value, expected) =>
   `setAttribute ${
     providedTitle ? providedTitle + " " : ""
-  }${key}=${value}`.trim();
+  }${JSON.stringify(object)} ${key}=${value} => ${JSON.stringify(expected)}`.trim();
 
 test(sat, {}, "a", 1, { a: 1 });
 test(sat, {}, "a.b", 1, { a: { b: 1 } });
+test(sat, { a: { b: "x" }}, "a.b", 1, { a: { b: 1 } });
 test(sat, { a: { x: 7 } }, "a.b.c.d", 1, { a: { x: 7, b: { c: { d: 1 } } } });
