1ac760efc602a6d0a8a347d42de53dc2879d0747
Markus Felten <Markus.Felten@gmx.de>
2022-02-09 00:05:51 +0100

    test: Project fullName

--- tests/project-test.mjs
+++ tests/project-test.mjs
@@ -2,10 +2,11 @@ import test from "ava";
 import { Project } from "repository-provider";
 
 test("init Project", async t => {
-  const owner = { _addProject: () => {} };
+  const owner = { name: "o1", _addProject: () => {} };
   const p = new Project(owner, "p1");
 
   t.is(p.owner, owner);
   t.is(p.name, "p1");
+  t.is(p.fullName, "o1/p1");
   t.is(p.displayName, "p1");
 });
