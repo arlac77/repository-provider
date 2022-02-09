9e3a1fcfefaf573b1e2bc2b22666fefa10132192
Markus Felten <Markus.Felten@gmx.de>
2022-02-09 11:07:01 +0100

    fix: given branch hav precedence over default

--- src/multi-group-provider.mjs
+++ src/multi-group-provider.mjs
@@ -50,7 +50,7 @@ export class MultiGroupProvider extends BaseProvider {
         if (rg !== undefined) {
           const r = await rg.repository(repository);
           if (r !== undefined) {
-            return r.branch( r.defaultBranchName || branch);
+            return r.branch( branch || r.defaultBranchName);
           }
         }
       }
