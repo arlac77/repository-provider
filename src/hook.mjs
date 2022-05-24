31e0a0e6453478b9e7ce2b17b9fe2738550588c7
Markus Felten <Markus.Felten@gmx.de>
2022-05-24 20:03:36 +0200

    fix: no dup id for hooks

--- src/hook.mjs
+++ src/hook.mjs
@@ -8,7 +8,6 @@ export class Hook extends OwnedObject {
   static get attributes() {
     return {
       ...super.attributes,
-      id: { type: "string", writable: true },
       url: { type: "url", description: "target url", writable: true },
       secret: { type: "string", private: true, writable: true },
       content_type: { type: "string", default: "json", writable: true },
@@ -34,3 +33,4 @@ export class Hook extends OwnedObject {
     return optionJSON(this, { name: this.name, id: this.id, events: [...this.events] });
   }
 }
+
