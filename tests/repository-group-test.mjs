1003a6ca894f5c26670ac19bc47d19959794bc30
Markus Felten <Markus.Felten@gmx.de>
2022-05-27 18:39:25 +0200

    fix: naming of repository grou

--- tests/repository-group-test.mjs
+++ tests/repository-group-test.mjs
@@ -8,8 +8,8 @@ import {
 } from "repository-provider";
 import { createMessageDestination } from "repository-provider-test-support";
 
-test("RepositoryGroup type", t => t.is(RepositoryGroup.type, "repositorygroup"));
-test("RepositoryGroup collection name", t => t.is(RepositoryGroup.collectionName, "repositorygroups"));
+test("RepositoryGroup type", t => t.is(RepositoryGroup.type, "repository-group"));
+test("RepositoryGroup collection name", t => t.is(RepositoryGroup.collectionName, "repositoryGroups"));
 
 test("repository-group create with options", t => {
   const provider = new MultiGroupProvider();
