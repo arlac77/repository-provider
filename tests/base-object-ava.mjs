e651054da68091258377e269de4d459cd3daade2
Markus Felten <Markus.Felten@gmx.de>
2023-01-05 15:19:00 +0100

    feat: updateAttributes

--- tests/base-object-ava.mjs
+++ tests/base-object-ava.mjs
@@ -2,9 +2,7 @@ import test from "ava";
 import { BaseObject } from "repository-provider";
 
 test("BaseObject writableAttributes", t => {
-  t.deepEqual(Object.keys(BaseObject.writableAttributes), [
-    "description"
-  ]);
+  t.deepEqual(Object.keys(BaseObject.writableAttributes), ["description"]);
 });
 
 test("BaseObject isWritable", t => {
@@ -16,4 +14,10 @@ test("BaseObject update", async t => {
   const b = new BaseObject();
   await b.update();
   t.true(true);
+});
+
+test("BaseObject updateAttributes", async t => {
+  const b = new BaseObject();
+  b.updateAttributes({ description: "my new description" });
+  t.is(b.description, "my new description");
 });
