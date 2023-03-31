5cd471c5a49d3891cf1b2f6a9016c6292ba7da66
Markus Felten <Markus.Felten@gmx.de>
2023-03-31 18:49:34 +0200

    style: reformat

--- tests/attributes-ava.mjs
+++ tests/attributes-ava.mjs
@@ -2,7 +2,7 @@ import test from "ava";
 import { boolean_attribute } from "repository-provider";
 
 test("boolean attribute", t => {
-    t.true(boolean_attribute.writeable);
-    t.false(boolean_attribute.default);
-    t.is(boolean_attribute.type, "boolean");
+  t.true(boolean_attribute.writeable);
+  t.false(boolean_attribute.default);
+  t.is(boolean_attribute.type, "boolean");
 });
