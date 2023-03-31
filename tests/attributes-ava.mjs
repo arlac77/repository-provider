29884713fedca0add79d1f39309ad225bef044c2
Markus Felten <Markus.Felten@gmx.de>
2023-03-31 11:16:58 +0200

    test: boolean default

--- tests/attributes-ava.mjs
+++ tests/attributes-ava.mjs
@@ -3,5 +3,6 @@ import { boolean_attribute } from "repository-provider";
 
 test("boolean attribute", t => {
     t.true(boolean_attribute.writeable);
+    t.false(boolean_attribute.default);
     t.is(boolean_attribute.type, "boolean");
 });
