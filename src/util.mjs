dda855fc30cee49650d5c6003806a7332235d246
Markus Felten <Markus.Felten@gmx.de>
2023-04-25 10:26:00 +0200

    fix: use es2020

--- src/util.mjs
+++ src/util.mjs
@@ -26,9 +26,7 @@ export function stripBaseName(name, repositoryBases, whenFound) {
     }
 
     if (name.startsWith(b)) {
-      if (whenFound) {
-        whenFound(b);
-      }
+      whenFound?.(b);
       return name.slice(b.length);
     }
   }
