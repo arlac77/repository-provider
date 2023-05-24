3465ebe83bfa3529672deb1fd61177181ff29c1b
Markus Felten <Markus.Felten@gmx.de>
2023-05-24 18:56:30 +0200

    fix: sync attribute

--- src/attribute.mjs
+++ src/attribute.mjs
@@ -19,21 +19,51 @@
  * @param {string} string
  * @yields {string}
  */
-function* tokens(string) {
+export function* tokens(string) {
   let identifier = "";
+  let last;
 
   for (const c of string) {
     switch (c) {
       case "\t":
       case " ":
+        if (identifier.length) {
+          yield identifier;
+          identifier = "";
+        }
+        if (last) {
+          yield last;
+          last = undefined;
+        }
         break;
 
+      case "!":
       case ">":
       case "<":
+        if (last) {
+          yield last;
+        }
+        last = c;
+        break;
 
+      case "=":
+        if (last) {
+          yield last + c;
+          last = undefined;
+          break;
+        }
+
+      case "+":
+      case "-":
       case ".":
+      case "(":
+      case ")":
       case "[":
       case "]":
+        if (last) {
+          yield last;
+          last = undefined;
+        }
         if (identifier.length) {
           yield identifier;
           identifier = "";
@@ -41,12 +71,19 @@ function* tokens(string) {
         yield c;
         break;
       default:
+        if (last) {
+          yield last;
+          last = undefined;
+        }
         identifier += c;
     }
   }
 
   if (identifier.length) {
     yield identifier;
+  }
+  if (last) {
+    yield last;
   }
 }
 
@@ -116,4 +153,48 @@ export function getAttribute(object, name) {
   }
 
   return object;
+}
+
+/**
+ * Deliver attribute value and operator.
+ * The name may be a property path like 'a.b.c <='.
+ * @param {Object} object
+ * @param {string} expression
+ * @returns {[any,string]} value associated with the given property name
+ */
+export function getAttributeAndOperator(object, expression, getters = {}) {
+  let op = "=";
+
+  for (const token of tokens(expression)) {
+    switch (token) {
+      case ">=":
+      case "<=":
+      case ">":
+      case "<":
+      case "=":
+      case "!=":
+        op = token;
+        break;
+      case ".":
+      case "[":
+      case "]":
+        break;
+
+      default:
+        if (object === undefined) {
+          break;
+        }
+
+        const g = getters[token];
+        if (g) {
+          object = g(object);
+        } else {
+          if (object[token] !== undefined) {
+            object = object[token];
+          }
+        }
+    }
+  }
+
+  return [object, op];
 }
